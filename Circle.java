095b5df7ec7958b62668d7af4796de82925a33bc
NisseOscar <o.n.johansson@gmail.com>
2018-11-30 13:30:15 +0100

    jbj

--- Circle.java
+++ Circle.java
@@ -4,11 +4,11 @@ import java.awt.Color;
 import java.awt.Graphics;
 /**
  * This class represents the shape of a circle.
- * It implements the interface GeometricalForm so that it defines basic characteristics 
- * Such as movability and geometric properties such as area. 
- * 
- * 
- * 
+ * It implements the interface GeometricalForm so that it defines basic characteristics
+ * such as movability and geometric properties such as area. 
+ *
+ *
+ *
  * @author Oscar and Martin
  *
  */
@@ -18,10 +18,10 @@ public class Circle implements GeometricalForm {
 	private Color color;
 	private int diameter;
 	private Oval wrappedOval;
-	
+
 	/**
 	 * Constructs a GeometricalForm in the shape of a circle.
-	 * 
+	 *
 	 * @param x
 	 * 			X coordinate of the circle.
 	 * @param y
@@ -36,10 +36,10 @@ public class Circle implements GeometricalForm {
 	public Circle(int x, int y, int diameter, Color c) throws IllegalPositionException{
 		wrappedOval = new Oval(x,y,diameter,diameter,c);
 	}
-	
+
 	/**
 	 * Constructs a GeometricalForm in the shape of a circle.
-	 * 
+	 *
 	 * @param f
 	 * 			GeometricalForm object, whose position is replicated.
 	 * @param diameter
@@ -50,93 +50,93 @@ public class Circle implements GeometricalForm {
 	public Circle(GeometricalForm f, int diameter, Color c) {
 		wrappedOval = new Oval(f,diameter,diameter,c);
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public int compareTo(GeometricalForm a) {
-		
+
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public void fill(Graphics g){
-		
+
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
     public Color getColor(){
-    	
+
     }
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
     public int getArea(){
-    	
+
     }
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
     public int getHeight(){
-    	
+
     }
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
     public int getPerimeter(){
-    	
+
     }
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
     public int getWidth(){
-    	
+
     }
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
     public int getX(){
-    	
+
     }
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
     public int getY(){
-    	
+
     }
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
     public void move(int dx, int dy) throws IllegalPositionException{
-    	
+
     }
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
     public void place(int x, int y) throws IllegalPositionException{
-    	
+
     }
 
 }
