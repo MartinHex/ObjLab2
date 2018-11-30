b6902c1ea7208611bfccb59b781fae696d06efc2
MartinHex <45486484+MartinHex@users.noreply.github.com>
2018-11-30 14:57:38 +0100

    Added some javadoc to the abstract class

--- AbstractForm.java
+++ AbstractForm.java
@@ -5,14 +5,34 @@ abstract class AbstractForm implements GeometricalForm{
   private int x;
   private int y;
   private Color c;
-
+  /**
+  *Constructs an AbstractForm that contains the basic properties for any form.
+  *
+  * @param x
+  *         X coordinate of the form.
+  * @param y
+  *         Y coordinate of the form.
+  * @param c
+  *         Color of the form.
+  * @throws IllegalPositionException
+            Only positive coordinates are allowed.
+  */
   protected AbstractForm(int x,int y,Color c) throws IllegalPositionException{
     if( x<0 || y<0){throw new IllegalPositionException();}
     this.x=x;
     this.y=y;
     this.c = c.copy();
   }
-  protected AbstractForm(GeometricalForm f) throws IllegalPositionException{
+
+  /**
+  *Constructs an AbstractForm that contains the basic properties for any form.
+  *
+  * @param f
+  *         A geometricalFrom from where the position is drawn. 
+  * @param c
+  *         Color of the form.
+  */
+  protected AbstractForm(GeometricalForm f, Color c){
     this.x=f.getX();
     this.y=f.getY();
     this.c = f.getColor().copy();
@@ -20,10 +40,12 @@ abstract class AbstractForm implements GeometricalForm{
   /**
   *{@inheritDoc}
   */
+  @Override
   public int getArea();
   /**
   *{@inheritDoc}
   */
+  @Override
   public int getPerimeter();
 
   public int compareTo( GeometricalForm f );
@@ -31,41 +53,48 @@ abstract class AbstractForm implements GeometricalForm{
   /**
   *{@inheritDoc}
   */
+  @Override
   public void fill( Graphics g );
 
   /**
   *{@inheritDoc}
   */
+  @Override
   public Color getColor(){
     return c.copy();
-  };
+  }
 
   /**
   *{@inheritDoc}
   */
+  @Override
   public int getWidth();
 
   /**
   *{@inheritDoc}
   */
+  @Override
   public int getHeight();
 
 
   /**
   *{@inheritDoc}
   */
+  @Override
   public int getX(){
 		return x;
   }
   /**
   *{@inheritDoc}
   */
+  @Override
   public int getY(){
 		return y;
   }
   /**
   *{@inheritDoc}
   */
+  @Override
   public void move(int dx, int dy) throws IllegalPositionException{
 		if( x+dx<0 || y+dy<0){throw new IllegalPositionException();}
 		this.x += dx;
@@ -74,6 +103,7 @@ abstract class AbstractForm implements GeometricalForm{
   /**
   *{@inheritDoc}
   */
+  @Override
   public void place(int x, int y) throws IllegalPositionException{
 		if( x<0 || y<0){throw new IllegalPositionException();}
 		this.x = x;
