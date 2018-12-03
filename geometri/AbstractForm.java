f28ae408411d2b7b509f4a914af2b05ecb17118e
MartinHex <45486484+MartinHex@users.noreply.github.com>
2018-12-03 10:28:40 +0100

    Added getHeight and getWidth to the absract class in accordance w/ correction

--- geometri/AbstractForm.java
+++ geometri/AbstractForm.java
@@ -5,6 +5,8 @@ abstract class AbstractForm implements GeometricalForm{
   private int x;
   private int y;
   private Color color;
+  private int height;
+  private int width;
   /**
   *Constructs an AbstractForm that contains the basic properties for any form.
   *
@@ -12,15 +14,21 @@ abstract class AbstractForm implements GeometricalForm{
   *         X coordinate of the form.
   * @param y
   *         Y coordinate of the form.
+  * @param height
+  *         Height of the form.
+  * @param width
+  *         Width of the form.
   * @param c
   *         Color of the form.
   * @throws IllegalPositionException
             Only positive coordinates are allowed.
   */
-  protected AbstractForm(int x,int y,Color c) throws IllegalPositionException{
+  protected AbstractForm(int x,int y, int height, int width, Color c) throws IllegalPositionException{
     if( x<0 || y<0){throw new IllegalPositionException();}
     this.x=x;
     this.y=y;
+    this.height = height;
+    this.width = width;
     this.color = c.clone();
   }
   /**
@@ -28,12 +36,18 @@ abstract class AbstractForm implements GeometricalForm{
   *
   * @param f
   *         GeometricalForm from where the coordinates are drawn.
+  * @param height
+  *         Height of the form.
+  * @param width
+  *         Width of the form.
   * @param c
   *         Color of the form.
   */
-  protected AbstractForm(GeometricalForm f,Color c) {
+  protected AbstractForm(GeometricalForm f, int height, int width, Color c) {
     this.x=f.getX();
     this.y=f.getY();
+    this.height = height;
+    this.width = width;
     this.color = c.clone();
   }
   /**
@@ -53,38 +67,42 @@ abstract class AbstractForm implements GeometricalForm{
   *{@inheritDoc}
   */
   @Override
-  public int getArea();
+  abstract int getArea();
   /**
   *{@inheritDoc}
   */
   @Override
-  public int getPerimeter();
+  abstract int getPerimeter();
 
   /**
   *{@inheritDoc}
   */
   @Override
-  public void fill( Graphics g );
+  abstract void fill( Graphics g );
 
   /**
   *{@inheritDoc}
   */
   @Override
   public Color getColor(){
-    return color.copy();
+    return color.clone();
   }
 
   /**
   *{@inheritDoc}
   */
   @Override
-  public int getWidth();
+  public int getWidth(){
+    return width;
+  }
 
   /**
   *{@inheritDoc}
   */
   @Override
-  public int getHeight();
+  public int getHeight(){
+    return height;
+  }
 
 
   /**
