a902acbe38d6a40f5e906f16c57d61e5429e8bc5
MartinHex <45486484+MartinHex@users.noreply.github.com>
2018-12-03 14:17:11 +0100

    Test

--- geometri/Testare.java
+++ geometri/Testare.java
@@ -4,8 +4,13 @@ import java.util.*;
 
 public class Testare{
 
+  private JFrame frame = new JFrame("Test");
+  Graphics g = frame.getGraphics();
+
   public static void main(String[] args){
-    
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+    frame.fillOval(0,0,40,40);
   }
 
 }
